9b5da1cb9547f9ca47b29591b868e8eff9496a50
ankala wu <729923753@qq.com>
2018-07-25 14:44:50 +0800

    Update quicksort.c

--- algorithms/quicksort.c
+++ algorithms/quicksort.c
@@ -3,25 +3,30 @@
 
 using namespace std;
 
-int partion(vector<int>&s, int left, int right){
-    int pivot = s[left];
-    int i=left,j = right;
-    while(i!=j){
-       while( i< j && s[j] > pivot) j--;
-       s[i] = s[j];
-       while(i<j && s[i] < pivot) i++;
-       s[j] = s[i];
-    }
-    s[i] = pivot;
-    return i;
+int partition(vector<int> &vi, int low, int up){
+        int pivot = vi[up];
+        int i = low-1;
+        for (int j = low; j < up; j++)
+        {
+                if(vi[j] <= pivot)
+                {
+                        i++;
+                        swap(vi[i], vi[j]);
+                }
+        }
+        swap(vi[i+1], vi[up]);
+        return i+1;
 }
 
-void quick_sort(vector<int>&s, int left, int right){
-     if(left>=right) return;
-     int mid = partion(s,left,right);
-     quick_sort(s,left,mid-1);
-     quick_sort(s,mid+1,right);
+void quickSort(vector<int> &vi, int low, int up){
+        if(low < up)
+        {
+                int mid = partition(vi, low, up);
+                quickSort(vi, low, mid-1);
+                quickSort(vi, mid+1, up);
+        }
 }
+
 
 int main(){
     int n;
@@ -31,7 +36,7 @@ int main(){
       vector<int> s(n);
       cout<<"Please enter your numbers:"<<endl;
       for (int i=0;i<n;i++)cin>>s[i];
-      quick_sort(s, 0, n-1);
+      quickSort(s, 0, n-1);
       for (int i=0;i<n;i++) cout<<s[i]<<" ";
       cout<<endl;
     }
